f25442d9bb6d8e6a30a899232d86b647b674003d
DAHN228 <danila16007@gmail.com>
2022-12-20 23:30:31 +0700

    уацшоцущхзшо

--- myshop/settings.py
+++ myshop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myshopdb',
+        'USER': 'shopadmin',
+        'PASSWORD': 'djangopasswd',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,33 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from shop.models import Customer, Order
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class CustomersListView(ListView):
+    template_name = 'customers.html'
+    model = Customer
+    context_object_name = 'customers_list'
+
+
+class OrdersListView(ListView):
+    template_name = 'orders.html'
+    model = Order
+    context_object_name = 'orders_list'
+
+
+class SearchView(ListView):
+    template_name = "search.html"
+    model = Order
+    context_object_name = 'orders_list'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Order.objects.filter(
+            Q(customer__first_name__icontains=query) | Q(customer__last_name__icontains=query)
+        ).order_by('order_date').reverse()
